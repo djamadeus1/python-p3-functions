c14bcf8f158e5cbcc056d74ab545450311f8c535
Ronald Roberts <djamadeus1@gmail.com>
2024-11-19 01:13:14 -0500

    Here we go

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
-def greet(name):
-    pass
+def greet(Naureen):
+    print(f"Hello, {Naureen}!")
 
-def greet_with_default(name="programmer"):
-    pass
+def greet_with_default(Sunny="programmer"):
+    print(f"Hello, {Sunny}!")
 
-def add(num1, num2):
-    pass
+def add(Num45, Num55):
+    return Num45 + Num55
 
 def halve(number):
-    pass
+    if not isinstance(number, (int, float)):
+        return None
+    return number / 2
+
+#def my_function(param):3
+   # print("Running myFunction")
+    #return param + 1
